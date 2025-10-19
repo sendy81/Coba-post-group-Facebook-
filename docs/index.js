e28e3b30fa43a9739d770de695eadc90d65203e5
sendy81 <sendimichan@gmail.com>
2025-10-19 21:00:44 +0700

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -250,8 +250,8 @@ try {
   }
 
   // Tambah buffer agar Facebook encode selesai
-  await page.waitForTimeout(15000);
-  console.log("⏳ Tambahan waktu encode 15 detik selesai");
+  await page.waitForTimeout(4000);
+  console.log("⏳ Tambahan waktu encode 4 detik selesai");
 
 } catch (e) {
   console.log("⚠️ Preview tidak muncul dalam batas waktu, paksa lanjut...");
